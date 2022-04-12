7ee836e4e2e4223e78d1193e5804b1b64dbf10cd
Slawek <slawomir.misz@edu.wsti.pl>
2022-04-12 17:57:48 +0200

    Setup firebase & activated react 18 render

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+
+root.render(
+  <App />
 );
 
